382dc28bf62838cb5b68ad838c3b49760860fead
Dominique <cyanthecomputerscientist@gmail.com>
2020-07-08 14:57:25 -0400

    Finsihed unit test Co-authored-by: devillejoshua14 <devillejoshua14@gmail.com>

--- src/main/java/com/codedifferently/stayready/Wufinancial.java
+++ src/main/java/com/codedifferently/stayready/Wufinancial.java
@@ -1,5 +1,80 @@
 package com.codedifferently.stayready;
 
 public class Wufinancial {
-    
+   public Double money = 0.0;
+
+
+    public Double dollarToEuro(Double money) //Converts a dollar to Euro
+    {
+        return 0.0;
+    }
+
+    public Double euroToDollar(Double cash) //Converts a euro to a Dollar
+    {
+        return 0.0;
+    }
+
+    public Double euroToPound(Double cash) //Converts a euro to a pound
+    {
+        return 0.0;
+    }
+    public Double PoundToDollar(Double cash)
+    {
+        return 0.0;
+    }
+    public Double poundToRupee(Double cash) //Converts a pound to a rupee
+    {
+        return 0.0;
+    }
+    public Double rupeeToDollar(Double cash)
+    {
+        return 0.0;
+    }
+    public Double rupeetoCanadianDollar(Double cash) //Converts a rupee to a canadian dollar
+    {
+        return 0.0;
+    }
+    public Double CanadianDollartoDollar(Double cash)
+    {
+        return 0.0;
+    }
+    public Double canadianDollartoSingaporeDollar(Double cash) //canadian dollar to a Singapore dollar
+    {
+        return 0.0; 
+    }
+    public Double SingaporeDollarToDollar(Double cash)
+    {
+        return 0.0;
+    }
+    public Double singaporeDollarToFranc(Double cash) //Singapore dollar to Franc
+    {
+        return 0.0;
+    }
+    public Double FrancToDollar(Double cash)
+    {
+        return 0.0;
+    }
+    public Double franctoRinggit(Double cash) //Franc to Ringgit 
+    {
+        return 0.0;
+    }
+    public Double ringgittoDollar(Double cash)
+    {
+        return 0.0;
+    }
+    public Double ringgitToYen(Double cash) //Ringgit To Yen
+    {
+        return 0.0;
+    }
+    public Double YuanRenmibiToDollar(Double cash)
+    {
+        return 0.0;
+    }
+
+    public Double yenToYuanRenmibi(Double cash) //Yen to Yuan Renmibi
+    {
+        return 0.0;
+    }
+
+
 }
--- src/test/java/com/codedifferently/WufinancialTest.java
+++ src/test/java/com/codedifferently/WufinancialTest.java
@@ -1,6 +1,147 @@
 package com.codedifferently;
 
+import com.codedifferently.stayready.Wufinancial;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
 public class WufinancialTest {
-    
+
+    @Test
+    public void dollarToEuro()
+    {
+        //Given 
+        Wufinancial wufinancial = new Wufinancial();
+        wufinancial.money = 5.0;
+        Double cash = wufinancial.money;
+        //When 
+        Double actual = wufinancial.dollarToEuro(cash);
+        Double expected = 4.7;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void euroToDollarTest()
+    {
+         //Given 
+         Wufinancial wufinancial = new Wufinancial();
+         wufinancial.money = 94.0;
+         Double cash = wufinancial.money;
+         //When 
+         Double actual = wufinancial.euroToDollar(cash);
+         Double expected = 100.0;
+         //Then
+         Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void euroToPoundTest()
+    {
+         //Given 
+         Wufinancial wufinancial = new Wufinancial();
+         wufinancial.money = 94.0;
+         Double cash = wufinancial.money;
+         //When 
+         Double actual = wufinancial.euroToPound(cash);
+         Double expected = 82.0;
+         //Then
+         Assert.assertEquals(expected, actual);
+        
+    }
+@Test
+    public void poundToRupeeTest()
+    {
+        //Given 
+        Wufinancial wufinancial = new Wufinancial();
+        wufinancial.money = 82.0;
+        Double cash = wufinancial.money;
+        //When 
+        Double actual = wufinancial.poundToRupee(cash);
+        Double expected = 6832.0;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void rupetoCanadianDollarTest()
+    {
+        //Given 
+        Wufinancial wufinancial = new Wufinancial();
+        wufinancial.money = 6832.0;
+        Double cash = wufinancial.money;
+        //When 
+        Double actual = wufinancial.rupeetoCanadianDollar(cash);
+        Double expected = 132.0;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void canadianDollartoSinaporeDollarTest()
+    {
+        //Given 
+        Wufinancial wufinancial = new Wufinancial();
+        wufinancial.money = 132.0;
+        Double cash = wufinancial.money;
+        //When 
+        Double actual = wufinancial.euroToDollar(cash);
+        Double expected = 143.0;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void SignaporeDollarstoFrancTest()
+    {
+        //Given 
+        Wufinancial wufinancial = new Wufinancial();
+        wufinancial.money = 143.0;
+        Double cash = wufinancial.money;
+        //When 
+        Double actual = wufinancial.euroToDollar(cash);
+        Double expected = 101.0;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void franctoRinggitTest()
+    {
+        //Given 
+        Wufinancial wufinancial = new Wufinancial();
+        wufinancial.money = 101.0;
+        Double cash = wufinancial.money;
+        //When 
+        Double actual = wufinancial.euroToDollar(cash);
+        Double expected = 447.0;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void ringgitToYenTest()
+    {
+        //Given 
+        Wufinancial wufinancial = new Wufinancial();
+        wufinancial.money = 447.0;
+        Double cash = wufinancial.money;
+        //When 
+        Double actual = wufinancial.euroToDollar(cash);
+        Double expected = 11584.0;
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void yenToYuanRenmibiTest()
+    {
+          //Given 
+          Wufinancial wufinancial = new Wufinancial();
+          wufinancial.money = 11584.0;
+          Double cash = wufinancial.money;
+          //When 
+          Double actual = wufinancial.euroToDollar(cash);
+          Double expected = 692.0;
+          //Then
+          Assert.assertEquals(expected, actual);
+    }
     
 }
